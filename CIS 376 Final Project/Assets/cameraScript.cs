d2ec91af65463063c25ae4ef819b523bf92ce5dd
Ethan Tron <tranet@mail.gvsu.edu>
2023-04-02 13:30:55 -0400

    Add Player movement

---
+++ CIS 376 Final Project/Assets/cameraScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    private float xRotation;
+    private float yRotation;
+    
+    public Transform orientation;
+
+    public float sensX;
+    public float sensY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
