b925221901cfea577903390ef44e0a1487fc8f0b
ari <arianacvngh@gmail.com>
2023-04-11 21:51:03 -0400

    added gun and bullet firing
    not polished but I want to commit it now so everyone can have it

--- CIS 376 Final Project/Assets/playerController.cs
+++ CIS 376 Final Project/Assets/playerController.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class playerController : MonoBehaviour
-{
-    
+/// <summary>
+/// Player controller
+/// </summary>
+public class playerController : MonoBehaviour {
+
+    #region Movement
     [Header("Movement")]
     public float moveSpeed;
-
     public float groundDrag;
-
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
+    
+    #endregion Movement
+
+
+    /// <summary> Gun bullets </summary>
+    public GameObject bullet;
+    public GameObject barrelEnd;
+    public GameObject gun;
 
     [HideInInspector] public float walkSpeed;
     [HideInInspector] public float sprintSpeed;
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode shootKey = KeyCode.F;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -36,19 +46,29 @@ public class playerController : MonoBehaviour
 
     Rigidbody rb;
 
-    private void Start()
-    {
+    protected float elapsedTime = 0;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start() {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
+        bullet = Resources.Load("Bullet", typeof(GameObject)) as GameObject;
         readyToJump = true;
     }
 
-    private void Update()
-    {
+
+    /// <summary>
+    /// Update player game object
+    /// </summary>
+    private void Update() {
+        elapsedTime += Time.deltaTime;
+
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        Debug.Log("Grounded: " + grounded + "");
+        //Debug.Log("Grounded: " + grounded + "");
         MyInput();
         SpeedControl();
 
@@ -59,62 +79,86 @@ public class playerController : MonoBehaviour
             rb.drag = 0;
     }
 
-    private void FixedUpdate()
-    {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void FixedUpdate() {
         MovePlayer();
     }
 
-    private void MyInput()
-    {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void MyInput() {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
         // when to jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
-        {
+        if (Input.GetKey(jumpKey) && readyToJump && grounded) {
             readyToJump = false;
 
             Jump();
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        if (Input.GetKeyDown(shootKey) && elapsedTime > 1.0f) {
+            Instantiate(bullet, barrelEnd.transform.position, barrelEnd.transform.rotation);
+            elapsedTime = 0;
+        }
     }
 
-    private void MovePlayer()
-    {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void MovePlayer() {
         // calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         // on ground
-        if(grounded)
+        if (grounded) {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
 
         // in air
-        else if(!grounded)
+        else if (!grounded) {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
     }
 
-    private void SpeedControl()
-    {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void SpeedControl() {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         // limit velocity if needed
-        if(flatVel.magnitude > moveSpeed)
-        {
+        if (flatVel.magnitude > moveSpeed) {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
 
-    private void Jump()
-    {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Jump() {
         // reset y velocity
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
-    private void ResetJump()
-    {
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ResetJump() {
         readyToJump = true;
     }
 }
