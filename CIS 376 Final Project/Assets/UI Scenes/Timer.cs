986b4987bef1c276fa82cd5246605a82c32e6caa
GVSU-Romandy-Vu <97295936+GVSU-Romandy-Vu@users.noreply.github.com>
2023-04-14 10:00:09 -0400

    Credits in main menu, imported more audio

---
+++ CIS 376 Final Project/Assets/UI Scenes/Timer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float currentTime;
+    public bool mainActive = true;
+    public UIDocument mainMenu;
+    public UIDocument credits;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentTime += Time.deltaTime;
+        if (currentTime >= 5)
+        {
+            mainActive = !mainActive;
+            if (mainActive)
+            {
+                ActivateMainMenu();
+            }
+            else
+            {
+                ActivateCredits();
+            }
+        }
+
+        if (Input.anyKey) { ActivateMainMenu(); }
+
+    }
+
+    void ActivateMainMenu()
+    {
+        currentTime = 0;
+        Debug.Log("Main Menu should be active.");
+        mainMenu.gameObject.SetActive(true);
+        credits.gameObject.SetActive(false);
+        mainActive = true;
+    }
+
+    void ActivateCredits()
+    {
+        currentTime = 0;
+        Debug.Log("Credits should be active.");
+        credits.gameObject.SetActive(true);
+        mainMenu.gameObject.SetActive(false);
+        mainActive = false;
+        
+    }
+}
