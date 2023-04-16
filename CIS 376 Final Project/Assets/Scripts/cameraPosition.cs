22e52a7411d22e16093651cd72ac519c31232d1a
ari <arianacvngh@gmail.com>
2023-04-16 13:56:59 -0400

    follow mouse with camera

--- CIS 376 Final Project/Assets/Scripts/cameraPosition.cs
+++ CIS 376 Final Project/Assets/Scripts/cameraPosition.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraPosition : MonoBehaviour
-{
+public class cameraPosition : MonoBehaviour {
     public Transform cameraPos;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         transform.position = cameraPos.position;
     }
 }
--- CIS 376 Final Project/Assets/Scripts/cameraScript.cs
+++ CIS 376 Final Project/Assets/Scripts/cameraScript.cs
@@ -2,25 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraScript : MonoBehaviour
-{
+public class cameraScript : MonoBehaviour {
     private float xRotation;
     private float yRotation;
-    
+
     public Transform orientation;
 
     public float sensX;
     public float sensY;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
