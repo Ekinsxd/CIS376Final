6b26f18074cef240c2c50a77b2bfe338f64edaf3
ari <arianacvngh@gmail.com>
2023-04-20 20:51:14 -0400

    rearranged bullet/powerup code
    added enemy bullet and player bullet to distinguish between layers

--- CIS 376 Final Project/Assets/Scripts/BulletController.cs
+++ CIS 376 Final Project/Assets/Scripts/BulletController.cs
@@ -7,9 +7,6 @@ public class BulletController : MonoBehaviour {
     Rigidbody rb;
     float lifeTime = 0.0f;
 
-    [HideInInspector] public BulletType bulletType;
-
-
     void Start() {
         rb = GetComponent<Rigidbody>();
     }
@@ -19,7 +16,7 @@ public class BulletController : MonoBehaviour {
     void Update() {
         // shoot bullet on first update
         if (lifeTime <= 0.0f) {
-            ShootBullet();
+            rb.AddForce(transform.forward * 75.0f, ForceMode.Impulse);
         }
 
         lifeTime += Time.deltaTime;
@@ -30,37 +27,11 @@ public class BulletController : MonoBehaviour {
 
 
     /// <summary>
-    /// Shoot bullet using <see cref="bulletType"/>
-    /// </summary>
-    /// <param name="type"> <see cref="BulletType"/> to shoot (assigned by Player) </param>
-    private void ShootBullet() {
-        float bulletSpeed = 75.0f;
-
-        switch (bulletType) {
-            case BulletType.M:
-                bulletSpeed = 200.0f;
-                break;
-
-            case BulletType.Normal:
-            case BulletType.S:
-            case BulletType.R:
-            case BulletType.F:
-                break;
-
-        }
-
-        //Debug.Log(bulletType);
-        //Debug.Log(transform.forward * bulletSpeed);
-        rb.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
-    }
-
-
-    /// <summary>
     /// 
     /// </summary>
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other) {
-        //Debug.Log("hit" + other.name + "!");
-        Destroy(gameObject);
+        if (other.tag != "Bullet")
+            Destroy(gameObject);
     }
 }
